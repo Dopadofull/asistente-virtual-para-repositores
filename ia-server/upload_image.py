7dd3ef8426ddedb25e479f869478cc3f277da5c9
Dopadofull <melquistar@gmail.com>
2024-11-26 18:56:04 -0300

    Avanzando con implementación de la IA al server

--- ia-server/upload_image.py
+++ ia-server/upload_image.py
@@ -4,18 +4,18 @@ import requests
 url = "http://127.0.0.1:8000/upload/"
 
 # Abre el archivo de imagen en modo binario
-file_path = "./manzana_roja.jpg"  # Cambia esto a la ruta de tu archivo de imagen
+file_path = "manzana_roja.jpg"  # Cambia esto a la ruta de tu archivo de imagen
 with open(file_path, "rb") as img_file:
     # Prepara el archivo para enviarlo en la solicitud POST
-    files = {"file": img_file, "filename": "hola"}  # Usa el tipo MIME correcto (por ejemplo, "image/jpeg" o "image/png")
+    files = {"file": img_file}  # Usa el tipo MIME correcto (por ejemplo, "image/jpeg" o "image/png")
     
     # Realiza la solicitud POST
     response = requests.post(url, files=files)
 
 # Verifica la respuesta
-if response.status_code == 200:
-    print("Imagen subida correctamente.")
-    print("Respuesta:", response.json())  # Imprime la respuesta JSON que devuelve la API
-else:
-    print(f"Error al subir la imagen: {response.status_code}")
-    print("Respuesta:", response.text)
+    if response.status_code == 200:
+        print("Imagen subida correctamente.")
+        print("Respuesta:", response.json())  # Imprime la respuesta JSON que devuelve la API
+    else:
+        print(f"Error al subir la imagen: {response.status_code}")
+        print("Respuesta:", response.text)
