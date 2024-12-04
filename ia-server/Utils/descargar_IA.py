b1a7a5d4aebc12e271a9da3c95bda0ad5e0fa41c
Dopadofull <melquistar@gmail.com>
2024-12-04 19:14:06 -0300

    Llegan las imagenes traqui

--- ia-server/Utils/descargar_IA.py
+++ ia-server/Utils/descargar_IA.py
@@ -1,15 +1,18 @@
 import subprocess
+from git import Repo
+import os
 
 def clonar_repositorio(url, ruta_destino):
     try:
         # Clona el repositorio en la ruta destino
-        subprocess.run(f"git clone {url}")
+        repo= Repo.clone_from(url, ruta_destino)
+        # cloned_repo.clone(ruta_destino)
         print(f"Repositorio clonado en: {ruta_destino}")
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
 # Ejemplo de uso
-url_repositorio = "https://github.com/Dopadofull/Tp_Final.git"
-ruta_destino = "/ruta/a/mi/carpeta/clonada"
+url_repositorio = "https://github.com/pjreddie/darknet"
+ruta_destino = "./repo"
 
 clonar_repositorio(url_repositorio, ruta_destino)
--- ia-server/main.py
+++ ia-server/main.py
@@ -1,28 +1,27 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse 
-
+from fastapi.responses import JSONResponse
 import shutil
 import os
 
-
-
 app = FastAPI()
 
-# Ruta para subir una image
+# Ruta para subir una imagen
 @app.post("/upload/")
 def upload_image(file: UploadFile = File(...)):
     try:
-        # Define el directorio donde se guardarán imagenes
+        # Define el directorio donde se guardarán las imágenes
         upload_dir = "upload_images"
-        os.makedirs(upload_dir, file.filename)
-
-        # Define el path completo donde se almacenara la imagen
-        file_path = os.path.join(upload_dir,file.filename)
-     
-        # guarda la imagen en el servidor
-        with open(file_path,"wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
         
+        # Crea el directorio si no existe
+        os.makedirs(upload_dir, exist_ok=True)
+
+        # Define el path completo donde se almacenará la imagen
+        file_path = os.path.join(upload_dir, file.filename)
+
+        # Guarda la imagen en el servidor
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
         return JSONResponse(content={"message": "Imagen recibida correctamente", "filename": file.filename}, status_code=200)
 
     except Exception as e:
@@ -31,6 +30,4 @@ def upload_image(file: UploadFile = File(...)):
 # Ruta principal
 @app.get("/")
 def read_root():
-    return{"Hola":"Mundo"}
-
-# Ruta IA para detección de objetos
+    return {"Hola": "Mundo"}
