37189237ba1ce6a965304b64caa0fb242a7af8bd
Dopadofull <melquistar@gmail.com>
2024-12-05 18:30:15 -0300

    codigo para poder utilizar la camara web BEBOTE

--- rapberry-app/src/camara.py
+++ rapberry-app/src/camara.py
@@ -1,10 +1,27 @@
 from dataclasses import dataclass
 from pathlib import Path as pl
+import cv2
 
 
 @dataclass
 class Camara:
     def sacar_fotos(self) -> pl:
         foto = pl("../data_fake/manzanin.jpeg")
+
+
+        cap = cv2.VideoCapture(0)
+
+        leido, frame = cap.read()
+
+        if leido == True:
+            cv2.imwrite("foto.png", frame)
+            print("Foto tomada correctamente")
+        else:
+            print("Error al acceder a la cámara")
+
+            
+        cap.release()
+
+
         foto.resolve()
         return foto
